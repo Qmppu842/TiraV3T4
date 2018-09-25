fcd244dab109e7b08bab2e2cb0fcef711d966915
Qmppu842 <omppu842@gmail.com>
2018-09-25 17:31:40 +0300

    Counting done, Quick to go. Still got that counting in me.

--- src/tirav3t4_not_a_countingsort/TiraV3T4_not_a_countingsort.java
+++ src/tirav3t4_not_a_countingsort/TiraV3T4_not_a_countingsort.java
@@ -1,7 +1,6 @@
 package tirav3t4_not_a_countingsort;
 
 import java.util.Arrays;
-import java.util.Queue;
 import java.util.Random;
 
 /**
@@ -13,6 +12,7 @@ public class TiraV3T4_not_a_countingsort {
     public static int size = 10;
     public static int offsetOfValues = 1;
     public static int valuesMax = 10;
+    public static int testThisManyTimes = 10000;
 
     /**
      * @param args the command line arguments
@@ -34,14 +34,24 @@ public class TiraV3T4_not_a_countingsort {
         countingSort(presistenceOfMemory);
         long dinosaursReborn = System.nanoTime();
 
+        /**
+         * Quick sort
+         */
         long timeToCompleteExams = sunDies - bigBang;
+        /**
+         * Arrays.sort sort
+         */
         long timeToFindJob = heatDeathOfUniverse - sunDies;
+        /**
+         * Counting sort
+         */
+        long timeToCountingSort = dinosaursReborn - heatDeathOfUniverse;
 
         // Validate tables
         boolean museumCurator = true;
 
         for (int i = 0; i < size; i++) {
-            if (clearlyMonaLisa[i] != fakeMonaLisa[i]) {
+            if (clearlyMonaLisa[i] != fakeMonaLisa[i] || presistenceOfMemory[i] != fakeMonaLisa[i]) {
                 museumCurator = false;
                 break;
             }
@@ -59,19 +69,28 @@ public class TiraV3T4_not_a_countingsort {
             System.out.println("MonaLisa: ");
             System.out.println(Arrays.toString(monaLisa));
             System.out.println();
-            System.out.println("End result competor: ");
+            System.out.println("End result Quick: ");
             System.out.println(Arrays.toString(clearlyMonaLisa));
             System.out.println("End result Arrays.sort: ");
             System.out.println(Arrays.toString(fakeMonaLisa));
+            System.out.println("End result Counting sort: ");
+            System.out.println(Arrays.toString(presistenceOfMemory));
+            System.out.println("_____________________________");
         }
         System.out.println();
         System.out.println("Lo(n)g time ago: ");
-        System.out.println("Competor: " + timeToCompleteExams);
-        System.out.println("Competor: " + timeToCompleteExams / 1000000 + "ms");
+        System.out.println("Quick: " + timeToCompleteExams);
+        System.out.println("Quick: " + timeToCompleteExams / 1000000 + "ms");
+        System.out.println();
+        System.out.println("Counting: " + timeToCountingSort);
+        System.out.println("Counting: " + timeToCountingSort / 1000000 + "ms");
         System.out.println();
         System.out.println("Arrays.sort: " + timeToFindJob);
         System.out.println("Arrays.sort: " + timeToFindJob / 1000000 + "ms");
         System.out.println();
+
+        System.out.println("_____________________________");
+        avg();
 
     }
 
@@ -79,26 +98,73 @@ public class TiraV3T4_not_a_countingsort {
 
     }
 
+    /**
+     * Knowing range of values it bit cheating. But Counting sort is still my
+     * favorite mind == blown sorting algorithm.
+     *
+     * @param presistenceOfMemory
+     */
     private static void countingSort(int[] presistenceOfMemory) {
+        int[] counts = new int[valuesMax - offsetOfValues + 1];
+        for (int i = 0; i < presistenceOfMemory.length; i++) {
+            int curr = presistenceOfMemory[i];
+            counts[curr - offsetOfValues] += 1;
+        }
+        int indexCounter = 0;
+        eka:
+        for (int i = 0; i < counts.length; i++) {
+            int curr = counts[i];
+            for (int j = 0; j < curr; j++) {
+                presistenceOfMemory[indexCounter] = i + offsetOfValues;
+                indexCounter++;
+                if (indexCounter > size) {
+                    break eka;
+                }
+            }
+        }
     }
 
-    private void avg() {
+    private static void avg() {
         long countingTime = 0;
         long quickTime = 0;
-        long ArraySort = 0;
+        long arraySort = 0;
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < testThisManyTimes; i++) {
             int[] data = generateData();
+            int[] asd = data.clone();
+            int[] zxc = data.clone();
+            int[] qwe = data.clone();
+            long eka = System.nanoTime();
+            countingSort(asd);
+            long toka = System.nanoTime();
+            quickSort(zxc);
+            long kolmas = System.nanoTime();
+            Arrays.sort(qwe);
+            long nelos = System.nanoTime();
+            countingTime += toka - eka;
+            quickTime += kolmas - toka;
+            arraySort += nelos - kolmas;
         }
+        long countingAvg = countingTime / testThisManyTimes;
+        long quickAvg = quickTime / testThisManyTimes;
+        long arraysAvg = arraySort / testThisManyTimes;
+        System.out.println("\033[31;1m" + "Avarage results: " + "\033[0m");
+        System.out.println("Counting avg time: " + countingAvg);
+        System.out.println("Counting avg time in ms: " + countingAvg / 1000000);
+        System.out.println("Quick avg time: " + quickAvg);
+        System.out.println("Quick avg time in ms: " + quickAvg / 1000000);
+        System.out.println("Arrays avg time: " + arraysAvg);
+        System.out.println("Arrays avg timein ms: " + arraysAvg / 1000000);
 
     }
 
-    private int[] generateData() {
+    private static int[] generateData() {
         int[] monaLisa = new int[size];
         Random rand = new Random();
         for (int i = 0; i < size; i++) {
             monaLisa[i] = rand.nextInt(valuesMax) + offsetOfValues;
         }
+        return monaLisa;
     }
 
 }
